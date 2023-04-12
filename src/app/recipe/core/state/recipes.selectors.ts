ee29b207332a32abd79d5e08ec94b467153a568d
Jb Fuss <jbfuss@foder.fr>
2023-04-12 17:45:51 +0200

    Recipe List - filter by name

--- src/app/recipe/core/state/recipes.selectors.ts
+++ src/app/recipe/core/state/recipes.selectors.ts
@@ -7,7 +7,13 @@ export const selectFilteredRecipes = createSelector(
     let filteredRecipes = recipes;
 
     if (filter.season) {
-      filteredRecipes = recipes.filter(r => r.season === filter.season);
+      filteredRecipes = filteredRecipes.filter(r => r.season === filter.season);
+    }
+
+    if (filter.name) {
+      filteredRecipes = filteredRecipes.filter(r => r.name.toLocaleLowerCase()
+        .includes(filter.name.toLocaleLowerCase())
+      );
     }
 
     return filteredRecipes;
